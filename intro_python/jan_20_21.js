e49929ccd70fd7407ab9bc3f956972c66355d877
Brittany Servent <bservent@gmail.com>
2021-01-20 13:12:07 -0800

    intro python notes

---
+++ intro_python/jan_20_21.js
@@ -0,0 +1,19 @@
+//WHY PYTHON? 
+//It is a simple programming language (reads very neanderthal like) and offers alot of structure when working with large quantities of data (much more than what shell scripts and batch files can offer)
+//It has a lot of "batteries included functionality" such as flexible arrays and dictionaries
+//It is an interpreted language which makes it easy to experiment and test functions during bottom-up program development
+
+//SYNTAX
+//uses new lines to complete code (no semicolons or parethesis)
+//use indentation to define scope of functions, loops, classes (other languages usually use curly brackets)
+//indentation is very impoortant = block of code
+//no variable command...you just declare it (ex: x = 5)
+
+//Elif and Else
+//the elif is saying "if this condition is not met than try this"
+//else catches anything that is left in the end of a conditional statements
+
+//FUNTIONS
+//a function is a block of code that is called to execute
+//it can retreive data and take in known parameters
+//in python def is used to define a function
