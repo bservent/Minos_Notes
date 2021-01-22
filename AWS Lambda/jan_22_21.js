a544f084ba7cfd9a28274d588d853b3a09abfd03
Brittany Servent <bservent@gmail.com>
2021-01-22 14:25:51 -0800

    AWS Lambds notes

---
+++ AWS Lambda/jan_22_21.js
@@ -0,0 +1,32 @@
+//AWS LAMBDA
+//pay as you use web service
+//must download AWS Lambda console
+//it is a function-as-a-service clound computing platform
+//computing platform that allows you to execute code in the cloud
+//is known for its high availability compute infrastructure
+//all you do is supply code in language that it supports
+//node.js, python, C#, Java, Go, .NET 
+
+//WHAT IS IT USED FOR?
+//web apps, websites, data and analytics, file processing
+
+//HOW IT WORKS
+//write code in Lambda's code editor 
+//code triggers AWS services
+//Lambda runs code using only the resources needed
+//pay for compute time you use
+
+//STRENGTHS
+//only pay for what you use
+//no infrastructure to maintain 
+//you can edit directly online which means you can work on multiple devices
+//connects to API Gateway and Other Connection Points
+//You can run AWS Lambda locally
+//strong security
+
+//WEAKNESSES
+//deployment package size is 50 MB max already zipped
+//max value for timeout is 900 seconds
+//cold start 
+//are short lived
+//only logs on CloudWatch
