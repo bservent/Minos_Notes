12736cf2cf81d6038a927a679805383564ef48ab
Brittany Servent <bservent@gmail.com>
2021-02-01 14:47:09 -0800

    week 2 notes for minos

---
+++ AWS Kinesis/aws_kinesis.py
@@ -0,0 +1,12 @@
+#AWS kinesis
+
+#managed scalable cloud-based service that streams large amounts of data per second
+#it is used to capture, store, and process large data streams in real time from things such as events logs and social media feeds
+#after processing data, Kineses distributes it to multiple consumers simultaneously
+#no need to batch up data - stream data to Kinesis as soon as its produced
+#stream real-time graphs
+#real-time data analytics
+
+#strengths = ability to process, huge loads of data, integrates well with loT Platform on Amazon, scalability, easy to learn, analyze data in real time, and fast streaming
+
+#weaknesses = not a queue system, so little visibility into "backlog" if there is any, and some issues with operating along with AWS lambda
---
+++ numpy_python/intro_numpy.py
@@ -0,0 +1,14 @@
+#What is NumPy
+
+#numerical python = python library useful for array
+#manipulation and complex mathemathical computations
+# N-dimensional arrays
+
+#Arrays
+#data structure that stores values of same data type
+#not python list because python list can store different data types
+
+#Create Array with NumPy
+#import numpy as np
+#np.array([])
+# see photo
