1b9839b5ad2b137a0ea3f3b3436361d4d5230376
Brittany Servent <bservent@gmail.com>
2021-02-05 15:23:29 -0800

    google cloud notes

---
+++ google_cloud/google_cloud.js
@@ -0,0 +1,9 @@
+//GOOGLE CLOUD
+
+//Setup
+//pay as you go software that must be downloaded with license
+//provider that computes resources for deploying and operating applications
+//you use a cloud platform when yu wnat the services you present to your users, your customers, or your fellow employees to be an application as opposed to a website
+//GCP use platform to as data center: to reach users worldwide, to borrow sophisticated analytics and AI functions, to utilize massive data storage, to take advantage of cost efficiencies
+//master server receives requests and alerts chunkserver - chunkserver sends data directly
+//
