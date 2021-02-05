d239eb740a78f898925a3ef962148d83e6e697c7
Brittany Servent <bservent@gmail.com>
2021-02-05 14:38:19 -0800

    machine learning notes

---
+++ ml_linear_regression_python/machine_learing.py
@@ -0,0 +1,16 @@
+#MACHINE LEARNING
+
+#Translates real life problems into data so that it can be numerically represented and clear
+#Quantify observations into numbers so the computer can understand find patterns that it can see
+
+#Strengths = following instructions, seeing patterns in large quatitavtive data sets, performs large amount of calculations without errors, transform data
+
+#Weaknesses = creating their own instructions, thinking critically
+
+#RGB = Red, Green, Blue = numbers for colors
+
+#Linear Regression
+
+#linear regression is continuous data(ex. temperature, height, what we will be focusing on)
+#classification is categorical data (ex. hot or cold, apples, oranges)
+#uses line to create relationship between two variables, one variable being the determining factor and the other being the dependent variable (ex. height and weight)
