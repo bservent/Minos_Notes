0bc79e757b3008755aa12319000b7922ef132b7a
Brittany Servent <bservent@gmail.com>
2021-02-05 15:10:03 -0800

    data_visualizations

---
+++ data_visualizations/data_visualizations.js
@@ -0,0 +1,9 @@
+//TYPES OF DATA
+
+//Visualizing data
+//Histograms, boxplots, scatter plots, bar charts, line plots, multiple line plots, 
+
+//Stylistic elements and guidelines
+//before choosing visual representation of data, you must know which trends, patterns and/or findings you want to show
+//line plots = usually used to represent time series data
+//choose simple way to convey information (no special effects)
