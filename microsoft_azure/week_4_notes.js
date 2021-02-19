9ae4a58eaff19e7bfad34502845885ad63468678
Brittany Servent <bservent@gmail.com>
2021-02-18 21:07:49 -0800

    azure notes

---
+++ microsoft_azure/week_4_notes.js
@@ -0,0 +1,27 @@
+//02_18_2021
+
+//MICROSOFT AZURE
+
+//pay to use service
+//12 month free trial
+//collection of cloud services
+//offers 4 diff forms of cloud computing: infrastructure as a service IaaS, platform as a service (PaaS), Software as a Service (SaaS) and serverless.
+
+//What is it used for?
+//running virtual machines or containers in the cloud is one of the most popular uses
+//supports Linux
+//host databases in cloud
+//backup and disaster recovery
+
+//Strengths
+// Azure easily helps businesses and organizations transition to the cloud.
+//Azure naturally functions well alongside other widely used Microsoft packages.
+//High availability
+//Data Security
+//Scalability
+//Cost-effective
+
+//Weaknesses
+//// Microsoft Azure has been shown to be more susceptible to outages in comparison to Google Cloud and AWS.
+// Users have complained about the quality of Microsoft’s technical support when handling issues with Azure.
+// Will require some expertise in order to operate efficiently.
